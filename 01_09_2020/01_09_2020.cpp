8ec93e704e177c5e61ce4061ba44bdf4ca0b150c
Slingreen <benjamindarici@hotmail.com>
2020-09-01 14:48:52 +0200

    FInal final version

--- 01_09_2020/01_09_2020.cpp
+++ 01_09_2020/01_09_2020.cpp
@@ -7,8 +7,10 @@ int main()
     std::cin >> age;
     if (age > 19)
         std::cout << "you are an adult\n";
-    else
+    else if (age > 10)
         std::cout << "You are a teenager\n";
+    else
+        std::cout << "you are a child\n";
 }
 /*    _
   .__(.)< (MEOW)
